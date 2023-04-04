ef3de3d08e38b15eb92c7755e9a68ed1dcd52378
Granko01 <109149912+Granko01@users.noreply.github.com>
2023-04-04 11:15:22 +0200

    AddedPhotonFusion_And_PhotonVoice

--- Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
@@ -77,7 +77,7 @@ namespace UnityStandardAssets.CrossPlatformInput
         private void CheckEnableControlRig()
         {
 #if MOBILE_INPUT
-		EnableControlRig(true);
+            EnableControlRig(true);
 #else
             EnableControlRig(false);
 #endif
@@ -86,10 +86,14 @@ namespace UnityStandardAssets.CrossPlatformInput
 
         private void EnableControlRig(bool enabled)
         {
-            foreach (Transform t in transform)
+            try
             {
-                t.gameObject.SetActive(enabled);
+                foreach (Transform t in transform)
+                {
+                    t.gameObject.SetActive(enabled);
+                }
             }
+            catch (Exception) { }
         }
 
 #if UNITY_EDITOR
